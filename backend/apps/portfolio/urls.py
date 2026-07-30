ddfe80ebc0054c5d9d763bec05a881af246429b1
Mohhamadaminn <mohamad4aminm@gmail.com>
2026-07-31 02:11:14 +0330

    changed the router address for PastEvent ViewSet

--- backend/apps/portfolio/urls.py
+++ backend/apps/portfolio/urls.py
@@ -6,6 +6,6 @@ router.register(r'artists', ArtistViewSet, basename='artist')
 router.register(r'artworks', ArtistWorkViewSet, basename='artistwork')
 router.register(r'courses', CourseViewSet, basename='course')
 router.register(r'meetings', MeetingViewSet, basename='meeting')
-router.register(r"gallery", PastEventViewSet, basename="pastevent")
+router.register(r'pastevents', PastEventViewSet, basename="pastevents")
 
 urlpatterns = router.urls
