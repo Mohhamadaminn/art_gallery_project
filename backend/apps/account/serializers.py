750b34671cc931bfb76626e906653693b3a9350e
Mohhamadaminn <mohamad4aminm@gmail.com>
2026-08-03 20:47:42 +0330

    added required email field to serizliers.py to get messages

--- backend/apps/account/serializers.py
+++ backend/apps/account/serializers.py
@@ -6,13 +6,19 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Profile
 
+from rest_framework.validators import UniqueValidator
+
 class SignupSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all(), message="This email is already registered.")]
+    )
     phone_number = serializers.CharField(write_only=True, max_length=20)
     age = serializers.IntegerField(write_only=True, required=False, allow_null=True)
 
     class Meta:
         model = User
-        fields = ['username', 'password', 'phone_number', 'age']
+        fields = ['username', 'password', 'email', 'phone_number', 'age']
         extra_kwargs = {'password': {'write_only': True}}
 
     def validate_age(self, value):
